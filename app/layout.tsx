a6ba867562fd4e68b41f9d5803f153c77bccf510
Alimjan <alimujiang1414@qq.com>
2023-12-20 11:57:10 +0800

    fix: typo issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter as FontSans } from "next/font/google"
+import { Inter } from 'next/font/google'
 import './globals.css'
 
 import { cn } from "@/lib/utils"
  
-export const fontSans:any = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Is - Ali',
@@ -23,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body  className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
+          inter.className
         )}>{children}</body>
     </html>
   )
