cee69ff11c48b39991237258d81b948d4a834a96
Alimjan <alimujiang1414@qq.com>
2024-01-04 11:39:14 +0800

    feat: add link to blog

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4">
-      alimjan ablimit
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <p>Welcome to the personal website of <span className=' text-black font-semibold'>Alimjan Ablimit</span>  (also known as Alimujiang Abudumiti).</p>
+
+      <p>My site is currently under construction, but in the meantime, you&apos;re warmly invited to learn more about me through my project blog.</p>
+
+      <p>For insights into my work and interests, please <Link className='text-sky-500 underline underline-offset-1' href={"https://alimjan.notion.site/Is-Ali-7b3453d3c328479da2a2a838c2deaefe"}>click here to explore my blog</Link>.</p>
+     
+      
     </main>
   )
 }
