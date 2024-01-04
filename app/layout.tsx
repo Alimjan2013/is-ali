b2d1af12ba0d77e572635011c6a5d5d278f4a8b1
Alimjan <alimujiang1414@qq.com>
2024-01-04 14:05:33 +0800

    feat: vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 import { cn } from "@/lib/utils"
  
@@ -21,7 +22,10 @@ export default function RootLayout({
       <body  className={cn(
           "min-h-screen bg-background font-sans antialiased",
           inter.className
-        )}>{children}</body>
+        )}>{children}
+          <Analytics />
+        
+        </body>
     </html>
   )
 }
