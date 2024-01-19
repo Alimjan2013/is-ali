3e3a7847e267487426ed5dcc0899be7ce8bb9058
Alimjan <alimujiang1414@qq.com>
2024-01-19 16:00:02 +0800

    feat: skills component

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -1,5 +1,12 @@
-export default function Cv(){
-return(
-    <div> this is CV</div>
-)
+import CVBlock from "./cvContent";
+
+export default function Cv() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold">Skills</h1>
+      <div>
+        <CVBlock></CVBlock>
+      </div>
+    </div>
+  );
 }
