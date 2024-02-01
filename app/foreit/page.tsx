8fcdbb5bd13f64efdebc8a2da867130d49a413d5
Alimjan <alimujiang1414@qq.com>
2024-02-01 15:30:48 +0800

    feat: new page for EIT application

---
+++ app/foreit/page.tsx
@@ -0,0 +1,20 @@
+export default function videoInfo() {
+  return (
+    <div className="flex-1 space-x-3 flex flex-row">
+      <div className="w-full h-full border-4 md:border-8  border-black">
+        <iframe
+          src="https://www.loom.com/embed/0281766fa2d04bb788eaf19e65135184"
+          allowFullScreen
+          className="w-full h-full p-2"
+        ></iframe>
+      </div>
+      <div className="w-full h-full border-4 md:border-8  border-black">
+        <iframe
+          className="border w-full h-full  p-2 "
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FzmKRCSDb2N0O7mttOOP7My%2FSalad-Say---english-learning-App---EIT-project%3Ftype%3Ddesign%26node-id%3D25-1011%26t%3DgAhxmILD0MufFEaS-1%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D25%253A1011%26mode%3Ddesign"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
+}
